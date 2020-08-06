b17e34e9d0d6f8d6e3cd8f4e07021de875381d22
joseangelcrn <jose.angel.cabeza.rn@gmail.com>
2020-08-06 15:36:31 +0200

    He actualizado el icono de la app con el logo del cocker

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,9 @@
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
 
+    <!-- Icono App -->
+    <link rel="shortcut icon" href="{{url('storage/sistema/cocker.jpg')}}" />
+
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
