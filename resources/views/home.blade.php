e92cc8abaa1ec8c5bc94102b3fe51e32aecce010
joseangelcrn <jose.angel.cabeza.rn@gmail.com>
2020-08-06 14:30:42 +0200

    Creacion de un pequeño menu para redirigirte a la creacion la subida de fichero o para ver los que tienes ya subido

---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row ">
+            <div class="col-12 text-center">
+                <a href="#" class="btn btn-secondary">Ver mis archivos subidos</a>
+                <a href="{{route('fichero.create')}}" class="btn btn-success">Subir fichero</a>
+            </div>
+        </div>
+    </div>
+@endsection
