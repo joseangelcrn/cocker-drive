cacd2be4e3846c8b66e3ad368bcd919d70e9a570
joseangelcrn <jose.angel.cabeza.rn@gmail.com>
2020-08-07 17:24:04 +0200

    Ruta para ver 'mis-ficheros' añadida al home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
     <div class="container">
         <div class="row ">
             <div class="col-12 text-center">
-                <a href="#" class="btn btn-secondary">Ver mis archivos subidos</a>
+                <a href="{{route('fichero.mis-ficheros')}}" class="btn btn-secondary">Ver mis archivos subidos</a>
                 <a href="{{route('fichero.create')}}" class="btn btn-success">Subir fichero</a>
             </div>
         </div>
