1e03403422896f54c4410962b5e59666b14a9ceb
joseangelcrn <jose.angel.cabeza.rn@gmail.com>
2020-08-07 12:32:06 +0200

    Importacion de axios.

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -30,6 +30,7 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
  */
 
 // import Echo from 'laravel-echo';
+import axios from 'axios'
 
 // window.Pusher = require('pusher-js');
 
