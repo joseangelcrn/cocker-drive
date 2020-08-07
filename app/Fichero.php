06a6fb0dea3e61a0bf041070cc298f975feffe26
joseangelcrn <jose.angel.cabeza.rn@gmail.com>
2020-08-07 12:30:07 +0200

    Funciones creadas :
    defaultDisk() = retornara el disco donde se almacenara los ficheros.
    
    getNombreFichero() = te devuelve el nombre del fichero a partir de un path.
    
    crearBin() = te crea el binario del fichero y lo almacena en el sotrage por defecto mencionado anteriormente.
    
    crearData() = te crea un nuevo registro en relacion al binario antteriormente guardado.

--- app/Fichero.php
+++ app/Fichero.php
@@ -4,12 +4,16 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Storage;
 
 class Fichero extends Model
 {
     //
     protected $table = "ficheros";
-    protected $fillable = ['nombre_real,nombre_hash,suser_id'];
+    protected $fillable = ['nombre_real','nombre_hash','user_id','extension'];
+
+    public static  $DIR_FICHEROS = '/ficheros';
 
     /**
      * Relaciones
@@ -24,14 +28,63 @@ class Fichero extends Model
      * Funciones
      */
 
-     public function crearBin(File $file)
-     {
+     /**
+      * retorna el disco por defecto que vamos a usar, en este caso el
+      * disco publico.
+      */
 
+     public static function defaultDisk()
+     {
+        return Storage::disk('public');
      }
 
-     public function crearData($nombreReal,$nombreHash,$userId)
+    /**
+     * Te devuelv e nombre del fichero (y su extension) a partir del path
+     */
+    public static  function getNombreFicheroByPath($path)
+    {
+        return substr($path, strrpos($path, '/') + 1);
+    }
+     /**
+      * Crea el binario del fichero en el disco por defecto
+      */
+     public static function crearBin($fichero)
      {
-         
+        $nombreReal = $fichero->getClientOriginalName();
+        $extension = $fichero->extension();
+        $fullNombreReal = $nombreReal.'.'.$extension;
+
+        $path = self::defaultDisk()->put(self::$DIR_FICHEROS,$fichero);
+
+        $nombreHash = self::getNombreFicheroByPath($path);
+
+        $resultado = array(
+            'nombre_hash'=> $nombreHash,
+            'nombre_real'=> $fullNombreReal,
+            'extension'=>$extension
+        );
+
+        return $resultado;
      }
+
+     /**
+      * Guarda la info en la base de datos
+      */
+
+     public static function crearData($nombreReal,$nombreHash,$extension,$userId)
+     {
+        $resultado = Fichero::create(
+            [
+            'nombre_real'=>$nombreReal,
+            'nombre_hash'=>$nombreHash,
+            'extension'=>$extension,
+            'user_id'=>$userId
+            ]
+
+        );
+
+        return $resultado;
+     }
+
 
 }
