9843bebce7bc36d74e81af7a68fd13f63c262860
joseangelcrn <jose.angel.cabeza.rn@gmail.com>
2020-08-08 14:07:27 +0200

    He cambiado la funcion que registra nuevos usuarios que trae laravel por defecto por la mia personalizada, la cual aporta mayor seguridad y provacidad para los archivos subidos. Dejo comentado la que venia por defecto por si tengo que volver por algun motivo.

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -64,10 +64,12 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
-            'name' => $data['name'],
-            'email' => $data['email'],
-            'password' => Hash::make($data['password']),
-        ]);
+        //personalizacion de la funcion crear usuario
+        return User::crear($data['name'],$data['email'],$data['password']);
+        // return User::create([
+        //     'name' => $data['name'],
+        //     'email' => $data['email'],
+        //     'password' => Hash::make($data['password']),
+        // ]);
     }
 }
