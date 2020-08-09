e2c165cd26e196d76986eb0fa32e777503467b31
joseangelcrn <jose.angel.cabeza.rn@gmail.com>
2020-08-09 14:35:26 +0200

    Ahora en la vista home se carga el uso total del disco de ese usuario.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Fichero;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +24,8 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $rootDir = auth()->user()->getRootDir();
+        $espacioTotalUsado = Fichero::getEspacioUsado($rootDir);
+        return view('home',compact('espacioTotalUsado'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,6 +4,9 @@
     <div class="container">
         <div class="row ">
             <div class="col-12 text-center">
+                <p class="text-white">Espacio total usado: <b>{{$espacioTotalUsado}} MB</b> </p>
+            </div>
+            <div class="col-12 text-center">
                 <a href="{{route('fichero.mis-ficheros')}}" class="btn btn-secondary">Ver mis archivos subidos</a>
                 <a href="{{route('fichero.create')}}" class="btn btn-success">Subir fichero</a>
             </div>
