f0f50c529e4cdd928866ba2da6d3b716173e5827
joseangelcrn <jose.angel.cabeza.rn@gmail.com>
2020-08-10 16:26:03 +0200

    Creation of gifloading

---
+++ resources/js/components/gif/GifLoading.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+       <img class="img-fluid" style="height:40px; width:40px;"  v-if="show" :src="'../storage/sistema/gifs/loading-dog.gif'" alt="Gif loading">
+    </div>
+</template>
+
+<script>
+    export default {
+         props:
+        {
+            'show':{
+                type:Boolean,
+                default:false
+            }
+        },
+        data(){
+            return{
+            }
+        },
+        beforeMount() {
+            this.fichero = this.$props.fichero_param;
+        }
+    }
+</script>
