74372fc82fe28b76f7b87fb40cde80069f644841
joseangelcrn <jose.angel.cabeza.rn@gmail.com>
2020-08-10 12:06:14 +0200

    Set title var of view

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>Cocker Drive</title>
+    <title>Cocker Drive - @yield('title')</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
