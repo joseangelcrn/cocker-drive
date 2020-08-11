d61dbb9695c80f81bff986f2d8a8bdf1cba5889e
joseangelcrn <jose.angel.cabeza.rn@gmail.com>
2020-08-11 22:28:50 +0200

    delete unused commented code

--- resources/views/fichero/mis-ficheros.blade.php
+++ resources/views/fichero/mis-ficheros.blade.php
@@ -10,21 +10,4 @@
             </div>
         </div>
     </div>
-    {{-- <div class="container">
-        <div class="row">
-            <div class="col-12">
-                <h1 class="text-center">Mis Archivos</h1>
-            </div>
-        </div>
-    </div> --}}
-    {{-- <div class="container">
-        <div class="row d-flex justify-content-center">
-            @foreach ($ficheros as $fichero)
-                <div class="col-lg-4 col-md-6 col-sm-12  my-2">
-                    <fichero-miniatura root_dir="{{auth()->user()->getRootDir()}}" fichero_param="{{$fichero->toJson()}}"></fichero-miniatura>
-                </div>
-            @endforeach
-        </div>
-    </div> --}}
-
 @endsection
