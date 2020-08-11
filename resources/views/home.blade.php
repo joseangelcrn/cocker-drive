aa8abf28a92c7354764e503fc855358b14e8afe0
joseangelcrn <jose.angel.cabeza.rn@gmail.com>
2020-08-11 13:34:20 +0200

    Now the home view load circle chart for quick look to see which files you have uploaded

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,15 +6,11 @@
     <div class="container">
         <div class="row ">
             <div class="col-12 text-center">
-                <p class="text-white">Espacio total usado: <b>{{$espacioTotalUsado}} MB</b> </p>
+                <a href="{{route('fichero.mis-ficheros')}}" class="btn btn-secondary">Ver mis archivos subidos</a>
+                <a href="{{route('fichero.create')}}" class="btn btn-success">Subir fichero</a>
             </div>
             <div class="col-12 text-center">
-                @if ($espacioTotalUsado > 0)
-                    <a href="{{route('fichero.mis-ficheros')}}" class="btn btn-secondary">Ver mis archivos subidos</a>
-                @else
-                    <button disabled class="btn btn-secondary">Ver mis archivos subidos</button>
-                @endif
-                <a href="{{route('fichero.create')}}" class="btn btn-success">Subir fichero</a>
+                <chart class="my-4" :size_disk_used="{{$sizeDiskUsed}}" :data_param="'{{json_encode($parsedData)}}'"></chart>
             </div>
         </div>
     </div>
