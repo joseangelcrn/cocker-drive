5f67389aa805cb4324c247a756e00d0df27e4298
joseangelcrn <jose.angel.cabeza.rn@gmail.com>
2020-08-15 10:48:00 +0200

    Command to clear temp directory ( that directory storage temporal files)

---
+++ app/Console/Commands/ClearTmpDir.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Filesystem\Filesystem;
+
+
+class ClearTmpDir extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'clear:temp';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Clear temp dir';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $file = new Filesystem;
+        $file->cleanDirectory(public_path('storage\\temp\\'));
+    }
+}
