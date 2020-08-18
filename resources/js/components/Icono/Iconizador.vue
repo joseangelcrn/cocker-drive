9a726499746ad04b7483c8d1aed75392ec4a3757
joseangelcrn <jose.angel.cabeza.rn@gmail.com>
2020-08-18 17:01:40 +0200

    adjusting styles to display shadow style only when this component is called since file creation

--- resources/js/components/Icono/Iconizador.vue
+++ resources/js/components/Icono/Iconizador.vue
@@ -1,32 +1,32 @@
 <template>
     <div>
       <!-- Si es Imagen pongo su imagen preview -->
-        <div v-if="extensions.image.includes(fichero.extension)" class="text-center sombra">
+        <div v-if="extensions.image.includes(fichero.extension)" class="text-center" :class="{'sombra':creating}">
             <img v-if="creating" style="height:250px;" class="img-thumbnail" :src="fichero.url" alt="fichero">
             <img v-else style="height:250px;" class="img-thumbnail" :src="'../storage/ficheros/'+root_dir+'/'+fichero.nombre_hash" alt="fichero">
         </div>
         <!-- Si es PDF: Icono PDF -->
-        <div v-else-if="fichero.extension == 'pdf'" class="text-center sombra">
+        <div v-else-if="fichero.extension == 'pdf'" class="text-center" :class="{'sombra':creating}">
             <img style="height:250px;" class="img-thumbnail" :src="'../storage/sistema/iconos/pdf.svg'" alt="Icono PDF">
         </div>
         <!-- Si es Doc/Docx: Icono Doc -->
-        <div v-else-if="extensions.document.includes(fichero.extension)" class="text-center sombra">
+        <div v-else-if="extensions.document.includes(fichero.extension)" class="text-center"  :class="{'sombra':creating}">
             <img style="height:250px;" class="img-thumbnail" :src="'../storage/sistema/iconos/doc.jpg'" alt="Icono PDF">
         </div>
         <!-- Si es TXT:  Icono Txt -->
-        <div v-else-if="fichero.extension == 'txt'" class="text-center sombra">
+        <div v-else-if="fichero.extension == 'txt'" class="text-center" :class="{'sombra':creating}">
             <img style="height:250px;" class="img-thumbnail" :src="'../storage/sistema/iconos/txt.png'" alt="Icono TXT">
         </div>
         <!-- Si es Musica -->
-        <div v-else-if="extensions.music.includes(fichero.extension)" class="text-center sombra">
+        <div v-else-if="extensions.music.includes(fichero.extension)" class="text-center" :class="{'sombra':creating}">
             <img style="height:250px;" class="img-thumbnail" :src="'../storage/sistema/iconos/music.png'" alt="Icono MUSIC">
         </div>
         <!-- Si es Video -->
-        <div v-else-if="extensions.video.includes(fichero.extension)" class="text-center sombra">
+        <div v-else-if="extensions.video.includes(fichero.extension)" class="text-center" :class="{'sombra':creating}">
             <img style="height:250px;" class="img-thumbnail" :src="'../storage/sistema/iconos/video.png'" alt="Icono VIDEO">
         </div>
         <!-- Si es Compressed File -->
-        <div v-else-if="extensions.compressed.includes(fichero.extension)" class="text-center sombra">
+        <div v-else-if="extensions.compressed.includes(fichero.extension)" class="text-center" :class="{'sombra':creating}">
             <img style="height:250px;" class="img-thumbnail" :src="'../storage/sistema/iconos/compressed.png'" alt="Icono VIDEO">
         </div>
 
