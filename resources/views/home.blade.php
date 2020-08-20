e3024d8304a8e7148870ed38625883b0ff55f642
joseangelcrn <jose.angel.cabeza.rn@gmail.com>
2020-08-20 10:22:03 +0200

    Replacing blade home for component vue home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,16 +3,15 @@
 @section('title','Home')
 
 @section('content')
-    <div class="container">
-        <div class="row ">
-            <div class="col-12 text-center">
-                <a href="{{route('fichero.mis-ficheros')}}" class="btn btn-secondary" title="Ver mis archivos"><i class="fas fa-list"></i></a>
-                <a href="{{route('fichero.create')}}" class="btn btn-success" title="Subir archivos"><i class="fas fa-cloud-upload-alt"></i></a>
-                <a href="{{route('fichero.download-all-files')}}" class="btn btn-warning" title="Descargar todos los archivo en carpeta comprimida."><i class="far fa-file-archive"></i></a>
-            </div>
-            <div class="col-12 text-center">
-                <chart-donut class="my-4" :size_disk_used="{{$sizeDiskUsed}}" :data_param="'{{json_encode($parsedData)}}'"></chart-donut>
-            </div>
-        </div>
-    </div>
+    <home
+        :size_disk_used="{{$sizeDiskUsed}}"
+        :data="{{json_encode($parsedData)}}"
+
+        :url_list_files="'{{route('fichero.mis-ficheros')}}'"
+        :url_upload_files="'{{route('fichero.create')}}'"
+        :url_download_all_compressed_files="'{{route('fichero.download-all-files')}}'"
+        :url_delete_all="'{{route('fichero.delete-all-files-current-user')}}'"
+
+        ></home>
 @endsection
+
