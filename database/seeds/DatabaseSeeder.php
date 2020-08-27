e01bbec5e2c0ea80ce752eebfa9d33859052a0e6
joseangelcrn <jose.angel.cabeza.rn@gmail.com>
2020-08-27 16:48:06 +0200

    Add new seeders (log type seeder)

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(UserSeeder::class);
+        $this->call(
+            [
+                UserSeeder::class,
+                LogTypeSeeder::class
+            ]
+        );
     }
 }
